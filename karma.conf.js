21cdc02c19cfe621df746295ab7b43924aae83fc
Blair Garrett <blair.garrett@sky.uk>
2016-07-14 23:39:49 +0100

    Added Istanbul and junit reporters for test coverage.

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,12 @@
 module.exports = function(config) {
   config.set({
+    basePath: '',
     plugins: [
       'karma-jasmine',
       'karma-coverage',
       'karma-webpack',
       'karma-phantomjs-launcher',
+      'karma-junit-reporter'
     ],
     browsers: ['PhantomJS'],
     files: [
@@ -12,11 +14,24 @@ module.exports = function(config) {
     ],
     frameworks: ['jasmine'],
     preprocessors: {
-      'tests.webpack.js': ['webpack']
+      'tests.webpack.js': ['webpack'],
+      'source/*.js*': ['coverage']
     },
-    reporters: ['dots', 'coverage'],
+    reporters: ['dots', 'progress', 'coverage', 'junit'],
     coverageReporter: {
-      type: 'text'
+      includeAllSources: true,
+      type : 'html',
+      dir : 'coverage'
+    },
+    // the default configuration
+    junitReporter: {
+      outputDir: 'junit/', // results will be saved as $outputDir/$browserName.xml
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: 'React-Autocomplete-String Tests', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: true, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {} // key value pair of properties to add to the <properties> section of the report
     },
     singleRun: true,
     webpack: {
@@ -33,10 +48,10 @@ module.exports = function(config) {
           { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
         ],
       },
-      watch: true,
+      watch: true
     },
     webpackServer: {
-      noInfo: true,
+      noInfo: true
     },
   });
 };
